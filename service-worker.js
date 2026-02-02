d737b38deadd91afcb24c969c2277deb7fb73a8d
timtimkaaa <mordashovtimofej@gmail.com>
2026-02-02 12:35:55 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -31,6 +31,11 @@ self.addEventListener('activate', (event) => {
     self.clients.claim();
 });
 
+/*
+  IMPORTANT:
+  Do NOT intercept fetch yet.
+  This prevents breaking Vite dev server.
+*/
 
 self.addEventListener('fetch', (event) => {
     const { request } = event;
